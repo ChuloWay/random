10284e28e8c01428c6b76530e79fadac3a7465c3
ChuloWay <okoyeebuka64@gmail.com>
2023-01-01 22:28:45 +0100

    custom interceptor & decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,38 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, UseInterceptors, ClassSerializerInterceptor, Session } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, UseInterceptors, ClassSerializerInterceptor, Session, BadRequestException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { UserDto } from './dtos/user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
-
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService,
         private authService: AuthService) { }
-    @Post('signup')
+
+    // @Get('/whoami')
+    // whoAmI(@Session() session:any) {
+    //     const user = this.usersService.findOne(session.userId)
+    //     if (!user) {
+    //         throw new BadRequestException('No User')
+    //     }
+    //     return user;
+    // }
+
+    @Get('/whoami')
+    whoAmI(@CurrentUser() user: string) {
+        return user;
+    }
+
+
+    @Post('/signout')
+    signOut(@Session() session:any) {
+        session.userId = null;
+    } 
+    @Post('/signup')
     async createUser(@Body() body: CreateUserDto, @Session() session: any) {
         const user = await this.authService.signup(body.email, body.password);
         session.userId = user.id
