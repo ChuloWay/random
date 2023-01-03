29aa966602103ac949b3af43c35e9a201ba528e3
ChuloWay <okoyeebuka64@gmail.com>
2023-01-03 23:30:50 +0100

    query strings bugfix

--- src/reports/dtos/get-est.dto.ts
+++ src/reports/dtos/get-est.dto.ts
@@ -1,5 +1,5 @@
 import { IsString, IsNumber, Min, Max, IsLatitude, IsLongitude, MAX } from "class-validator";
-
+import { Transform } from "class-transformer";
 export class GetEstimateDto {
     @IsString()
     make: string;
@@ -7,19 +7,23 @@ export class GetEstimateDto {
     @IsString()
     model: string;
 
+    @Transform(({ value }) => parseInt(value) )
     @IsNumber()
     @Min(1930)
     @Max(2022)
     year: number;
 
+    @Transform(({ value }) => parseInt(value) )
     @IsNumber()
     @Min(0)
     @Max(100000)
     mileage: number;
 
+    @Transform(({ value }) => parseFloat(value) )
     @IsLongitude()
     lng: number;
 
+    @Transform(({ value }) => parseFloat(value) )
     @IsLatitude()
     lat: number;
 
