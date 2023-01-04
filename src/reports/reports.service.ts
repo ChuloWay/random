02c86bf2aa9ee3867cee12b74e536c516d1cb8f9
ChuloWay <okoyeebuka64@gmail.com>
2023-01-04 18:32:10 -0400

    Query Builder

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,10 +4,27 @@ import { UserEntity } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportEntity } from './reports.entity';
+import { GetEstimateDto } from './dtos/get-est.dto';
 
 @Injectable()
 export class ReportsService {
     constructor(@InjectRepository(ReportEntity) private repo: Repository<ReportEntity>) { }
+
+    createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+        return this.repo
+          .createQueryBuilder()
+          .select('AVG(price)', 'price')
+          .where('make = :make', { make })
+          .andWhere('model = :model', { model })
+          .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+          .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+          .andWhere('year - :year BETWEEN -3 AND 3', { year })
+          .andWhere('approved IS TRUE')
+          .orderBy('ABS(mileage - :mileage)', 'DESC')
+          .setParameters({ mileage })
+          .limit(3)
+          .getRawOne();
+      }
 
     create(reportDto: CreateReportDto, user: UserEntity) {
         const report = this.repo.create(reportDto);
